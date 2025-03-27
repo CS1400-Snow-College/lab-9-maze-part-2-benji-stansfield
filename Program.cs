753f9e4f25f95f418d80613ad94b29b33e685bd2
Benji Stansfield <benji.stansfield@students.snow.edu>
2025-03-27 11:48:35 -0600

    Intro screen with instructions

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿/*Benji Stansfield, 3-27-25, Lab 9 "Maze 2"*/
+Console.Clear();
+
+/*Intro screen*/
+Console.WriteLine(@"-------------
+ MAZE GAME 2
+-------------
+");
+
+Console.WriteLine(@"Avoid the bad guys (%) and collect as much money (^ or $) as possible. Unlock the door to reach the '#' as fast as possible.
+Press any key to begin.");
+Console.ReadKey(true);
+Console.Clear();
